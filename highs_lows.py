e188633a3a9a3d63e3f9ef6582738d192fbeb5fd
Henry Pai / Pasona N A, Inc. <hpai@pasona.com>
2024-07-30 17:16:53 -0700

    Imported matplotlib. Created first graph (Temperature July 2014)

--- highs_lows.py
+++ highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+from matplotlib import pyplot as plt
 
 file = 'sitka_weather_07-2014.csv'
 with open(file) as f:
@@ -14,4 +15,14 @@ with open(file) as f:
         high = int(row[1])
         highs.append(high)
     
-    print(highs)
+    # Plot Data
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+    plt.plot(highs, c='red')
+
+    # Format plot
+    plt.title("Daily high temperatures, July 2014", fontsize=24)
+    plt.xlabel('', fontsize=16)
+    plt.ylabel("Temperature (F)", fontsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=16)
+
+    plt.show()
